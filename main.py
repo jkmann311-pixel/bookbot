4c4b9b272c43674083ba1d96454463d89df49164
jkmann311-pixel <jkmann311@gmail.com>
2025-10-31 17:54:32 -0500

    C: Finished Program

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from stats import word_count
+from stats import word_count, character_count, sort_characters
+import sys
 
 def main():
-    #book_text = get_book_text('books/frankenstein.txt')
-    #return print(book_text)
-    words = word_count('books/frankenstein.txt')
-    return print(f"Found {words} total words")
+    filepath = sys.argv[1]
+    words = word_count(filepath)
+    ch = character_count(filepath)
+    new_list = sort_characters(ch)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {words} total words")
+    print("--------- Character Count -------")
+    for d in new_list:
+        ch_key = list(d.keys())[0]
+        count = list(d.values())[0]
+        print(f"{ch_key}: {count}")
 
+    print("============= END ===============")
 
-main()
+    return
+
+if len(sys.argv) == 1:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+else:
+    main()
--- stats.py
+++ stats.py
@@ -1,14 +1,31 @@
-# takes a filepath as an input, specifically a filepath to a text file. then it opens and reads that text file to a string.
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
-
     return file_contents
 
 def word_count(filepath):
-    # takes the text from the book and stores it in a string
     book_text = get_book_text(filepath)
-    # i want to get that string and count how many words there are. maybe len(split(book_text))
     number_of_words = len(book_text.split())
-    
     return number_of_words
+
+def character_count(filepath):
+    characters = {}
+    book_text = get_book_text(filepath).lower()
+    for ch in book_text:
+        if ch.isalpha():
+            if ch in characters:
+                characters[ch] += 1
+            else:
+                characters[ch] = 1
+        else:
+            pass
+    return characters
+
+def sort_on(d):
+    return list(d.values())[0]
+
+def sort_characters(characters):
+    char_list = [{key: value} for key, value in characters.items()]
+    char_list.sort(key=sort_on, reverse=True)
+
+    return char_list
